2556215989e1e59d159ebb2b19a63344dc27e714
Ivan Egorov <egorich.3.04@gmail.com>
2016-02-14 16:46:12 +0100

    Pass most args by value

--- go/src/raytracer/raytracer.go
+++ go/src/raytracer/raytracer.go
@@ -18,19 +18,19 @@ type Vec3 struct {
 func (v *Vec3) norm() float64 { return math.Sqrt(v.x*v.x + v.y*v.y + v.z*v.z) }
 
 // Vector operations.
-func Add(a, b *Vec3) *Vec3          { return &Vec3{a.x + b.x, a.y + b.y, a.z + b.z} }
-func Sub(a, b *Vec3) *Vec3          { return &Vec3{a.x - b.x, a.y - b.y, a.z - b.z} }
-func Neg(a *Vec3) *Vec3             { return &Vec3{-a.x, -a.y, -a.z} }
-func Mult(a float64, b *Vec3) *Vec3 { return &Vec3{a * b.x, a * b.y, a * b.z} }
-func Div(a *Vec3, b float64) *Vec3  { return &Vec3{a.x / b, a.y / b, a.z / b} }
-func DotProduct(a, b *Vec3) float64 { return a.x*b.x + a.y*b.y + a.z*b.z }
-func CrossProduct(a, b *Vec3) *Vec3 {
-	return &Vec3{
+func Add(a, b Vec3) Vec3           { return Vec3{a.x + b.x, a.y + b.y, a.z + b.z} }
+func Sub(a, b Vec3) Vec3           { return Vec3{a.x - b.x, a.y - b.y, a.z - b.z} }
+func Neg(a Vec3) Vec3              { return Vec3{-a.x, -a.y, -a.z} }
+func Mult(a float64, b Vec3) Vec3  { return Vec3{a * b.x, a * b.y, a * b.z} }
+func Div(a Vec3, b float64) Vec3   { return Vec3{a.x / b, a.y / b, a.z / b} }
+func DotProduct(a, b Vec3) float64 { return a.x*b.x + a.y*b.y + a.z*b.z }
+func CrossProduct(a, b Vec3) Vec3 {
+	return Vec3{
 		a.y*b.z - b.y*a.z,
 		a.z*b.x - b.z*a.x,
 		a.x*b.y - b.x*a.y}
 }
-func Normalize(a *Vec3) *Vec3 { return Div(a, a.norm()) }
+func Normalize(a Vec3) Vec3 { return Div(a, a.norm()) }
 
 type Ray struct {
 	// dir should be normalized
@@ -39,14 +39,14 @@ type Ray struct {
 
 type Shape interface {
 	// Returns color of the shape in the specified point.
-	color(p *Vec3) *Vec3
+	color(p Vec3) Vec3
 	// Returns normal vector to the shape in the specified point.
-	n(p *Vec3) *Vec3
+	n(p Vec3) Vec3
 	// If the ray intersects the shape, shall return the distance from
 	// the origin to the closest intersection point. That is:
 	//   r.origin + d*r.dir == intersection_point
 	// Otherwise may return negative number, or +-Inf.
-	RayIntersect(r *Ray) float64
+	RayIntersect(r Ray) float64
 }
 
 type Sphere struct {
@@ -55,11 +55,11 @@ type Sphere struct {
 	color_ Vec3
 }
 
-func (s *Sphere) color(p *Vec3) *Vec3 { return &s.color_ }
-func (s *Sphere) n(p *Vec3) *Vec3     { return Normalize(Sub(p, &s.center)) }
-func (s *Sphere) RayIntersect(r *Ray) float64 {
-	a := Sub(&s.center, &r.origin)
-	p := DotProduct(a, &r.dir)
+func (s *Sphere) color(p Vec3) Vec3 { return s.color_ }
+func (s *Sphere) n(p Vec3) Vec3     { return Normalize(Sub(p, s.center)) }
+func (s *Sphere) RayIntersect(r Ray) float64 {
+	a := Sub(s.center, r.origin)
+	p := DotProduct(a, r.dir)
 
 	d2 := DotProduct(a, a) - p*p
 	r2 := s.radius * s.radius
@@ -86,7 +86,7 @@ type Plane struct {
 }
 
 func MakePlane(center, n, color Vec3) *Plane {
-	n_ := Normalize(&n)
+	n_ := Normalize(n)
 	// construct an orthogonal vector to n_.
 	var x_ Vec3
 	if math.Abs(n_.x) < kEps {
@@ -96,30 +96,30 @@ func MakePlane(center, n, color Vec3) *Plane {
 	} else if math.Abs(n_.z) < kEps {
 		x_ = Vec3{0, 0, 1}
 	} else {
-		x_ = *Normalize(&Vec3{-n_.y, n.x, 0})
+		x_ = Normalize(Vec3{-n_.y, n.x, 0})
 	}
 	//
-	y_ := *CrossProduct(n_, &x_)
-	return &Plane{center, *n_, x_, y_, color}
+	y_ := CrossProduct(n_, x_)
+	return &Plane{center, n_, x_, y_, color}
 }
 
-func (s *Plane) color(p *Vec3) *Vec3 {
-	dir := Sub(p, &s.center_)
-	x_coord := int(DotProduct(dir, &s.x_))
-	y_coord := int(DotProduct(dir, &s.y_))
+func (s *Plane) color(p Vec3) Vec3 {
+	dir := Sub(p, s.center_)
+	x_coord := int(DotProduct(dir, s.x_))
+	y_coord := int(DotProduct(dir, s.y_))
 	x_mod := (x_coord%100+100)%100 - 50
 	y_mod := (y_coord%100+100)%100 - 50
 	if x_mod*y_mod < 0 {
-		return &s.color_
+		return s.color_
 	}
-	return Mult(0.5, &s.color_)
+	return Mult(0.5, s.color_)
 }
 
-func (s *Plane) n(p *Vec3) *Vec3 { return &s.n_ }
+func (s *Plane) n(p Vec3) Vec3 { return s.n_ }
 
-func (s *Plane) RayIntersect(r *Ray) float64 {
-	p1 := DotProduct(&r.dir, &s.n_)
-	p2 := DotProduct(Sub(&r.origin, &s.center_), &s.n_)
+func (s *Plane) RayIntersect(r Ray) float64 {
+	p1 := DotProduct(r.dir, s.n_)
+	p2 := DotProduct(Sub(r.origin, s.center_), s.n_)
 	if math.Abs(p2) < kEps {
 		return 0
 	}
@@ -130,7 +130,7 @@ func (s *Plane) RayIntersect(r *Ray) float64 {
 }
 
 // Simple 1-step trace.
-func trace(r *Ray, scene []Shape, l *Vec3) *Vec3 {
+func trace(r Ray, scene []Shape, l Vec3) Vec3 {
 	// index of closest intersecting shape, and distance to it.
 	index := 0
 	distance := math.Inf(1)
@@ -139,8 +139,7 @@ func trace(r *Ray, scene []Shape, l *Vec3) *Vec3 {
 		if shape == nil {
 			continue
 		}
-		sd := shape.RayIntersect(r)
-		if sd >= 0 && sd < distance {
+		if sd := shape.RayIntersect(r); sd >= 0 && sd < distance {
 			distance = sd
 			index = t
 		}
@@ -148,12 +147,12 @@ func trace(r *Ray, scene []Shape, l *Vec3) *Vec3 {
 
 	if math.IsInf(distance, 0) {
 		// No intersection, return black.
-		return &Vec3{}
+		return Vec3{}
 	}
 
 	// Reflect the ray back to the source of light.
 	// We shift r_point by (1-kEps) towards us to avoid effects caused by rounding errors.
-	r_point := Add(&r.origin, Mult(distance*(1.0-kEps), &r.dir))
+	r_point := Add(r.origin, Mult(distance*(1.0-kEps), r.dir))
 	light_vec := Sub(l, r_point)
 	light_distance := light_vec.norm()
 	light_dir := Normalize(light_vec)
@@ -164,9 +163,9 @@ func trace(r *Ray, scene []Shape, l *Vec3) *Vec3 {
 		if shape == nil {
 			continue
 		}
-		sd := shape.RayIntersect(&Ray{*r_point, *light_dir})
 
 		// If there's an object between us and light source, then alarm!
+		sd := shape.RayIntersect(Ray{r_point, light_dir})
 		if sd >= 0 && sd < light_distance {
 			distance = sd
 			break
@@ -189,32 +188,32 @@ func trace(r *Ray, scene []Shape, l *Vec3) *Vec3 {
 			scene[index].color(r_point)))
 }
 
-func render(width, height int, camera_depth float64, l *Vec3, scene []Shape) *[]Vec3 {
+func render(width, height int, camera_depth float64, l Vec3, scene []Shape) []Vec3 {
 	// Iterate over the scene, generate 2*width x 2*height picture with 0,0 in its center.
 	result := make([]Vec3, 4*width*height)
 
-	camera := &Vec3{0, 0, -camera_depth}
+	camera := Vec3{0, 0, -camera_depth}
 	for x := -width; x < width; x++ {
 		for y := -height; y < height; y++ {
-			ray_dir := Normalize(Sub(&Vec3{float64(x), float64(y), 0}, camera))
-			color := trace(&Ray{*camera, *ray_dir}, scene, l)
-			result[(y+height)*2*width+(x+width)] = *color
+			ray_dir := Normalize(Sub(Vec3{float64(x), float64(y), 0}, camera))
+			color := trace(Ray{camera, ray_dir}, scene, l)
+			result[(y+height)*2*width+(x+width)] = color
 		}
 	}
-	return &result
+	return result
 }
 
-func read_vector() *Vec3 {
+func read_vector() Vec3 {
 	var x, y, z float64
 	fmt.Scan(&x, &y, &z)
 	// invert y component to make it look more natural
-	return &Vec3{x, -y, z}
+	return Vec3{x, -y, z}
 }
 
-func read_color() *Vec3 {
+func read_color() Vec3 {
 	var x, y, z int
 	fmt.Scan(&x, &y, &z)
-	return &Vec3{float64(x), float64(y), float64(z)}
+	return Vec3{float64(x), float64(y), float64(z)}
 }
 
 func main() {
@@ -238,19 +237,19 @@ func main() {
 			var radius float64
 			fmt.Scan(&radius)
 			color := read_color()
-			scene[i] = &Sphere{*center, radius, *color}
+			scene[i] = &Sphere{center, radius, color}
 		case "P":
 			center := read_vector()
 			n := read_vector()
 			color := read_color()
-			scene[i] = MakePlane(*center, *n, *color)
+			scene[i] = MakePlane(center, n, color)
 		}
 	}
 
 	ren := render(width, height, camera_depth, light_source, scene)
 
 	fmt.Print("P3 ", 2*width, " ", 2*height, " 255\n")
-	for _, c := range *ren {
+	for _, c := range ren {
 		fmt.Print(int(c.x), " ", int(c.y), " ", int(c.z), "\n")
 	}
 }
